4ab18b65104cf7aac9eac480397e0396f32e4cc1
Alexander Lytkin <a.lytkin@omprussia.ru>
2022-01-23 16:59:16 +0300

    Fix after review

--- third_homework/json_converter.py
+++ third_homework/json_converter.py
@@ -8,41 +8,59 @@ def json_converter():
         csv_reader = csv.DictReader(books)
         for row in csv_reader:
             book_list.append(row)
+    return book_list
 
+
+def write_book_json(book_list):
     with open('books.json', 'w') as books_json:
         json.dump(book_list, books_json, indent=4)
 
+
+def read_book_dict_json():
     with open('books.json') as books_dictionary_json:
         books_data = json.load(books_dictionary_json)
-        users_with_books = []
+    return books_data
 
+
+def read_user_dict_json():
     with open('users.json', 'r') as user_json:
         users_data = json.load(user_json)
+    return users_data
 
-    def gen_func():
-        for user in users_data:
-            yield user
 
-    fun = gen_func()
-
+def iter_books(books_data):
     for book in books_data:
+        yield book
+
+
+def iter_users(users_data):
+    for user in users_data:
+        users = {
+            'name': user["name"],
+            'gender': user["gender"],
+            'address': user["address"],
+            'age': user["age"],
+        }
+        yield users
+
+
+def add_book_to_user(books_data, users_data):
+    fun_book = iter_books(books_data)
+    fun_user = iter_users(users_data)
+    draft_user = []
+    for i in range(len(books_data)):
         try:
-            user = next(fun)
+            book_dict = next(fun_book)
+            user = next(fun_user)
+            dictionary_copy = user.copy()
+            dictionary_copy["books"] = [book_dict]
+            draft_user.append(dictionary_copy)
         except StopIteration:
-            fun = gen_func()
-
-        data = {'name': user["name"],
-                'gender': user["gender"],
-                'address': user["address"],
-                'age': user["age"],
-                'books': [
-                    {'title': book["Title"],
-                     'author': book["Author"],
-                     "pages": book["Pages"],
-                     "genre": book["Genre"],
-                     }]
-                }
-        users_with_books.append(data)
-
+            try:
+                for i in draft_user:
+                    other_books = next(fun_book)
+                    i['books'].append(other_books)
+            except StopIteration:
+                pass
     with open('result.json', 'w') as result:
-        json.dump(users_with_books, result, indent=4)
+        json.dump(draft_user, result, indent=4)
