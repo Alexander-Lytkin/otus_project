f49992f5f756cb88dbe666c2ad0a3585f9bb1bad
Alexander Lytkin <a.lytkin@omprussia.ru>
2022-04-16 17:31:00 +0300

    Update tests

--- seven_homework/tests/test_login_page.py
+++ seven_homework/tests/test_login_page.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -6,12 +7,13 @@ from seven_homework.page_objets.LoginPage import LoginAdminPage
 
 
 @allure.title("Проверка наличия элементов страницы для входа")
+@pytest.mark.skip(reason="use only with local opencard")
 def test_check_admin_page_elements(browser):
     browser.get(browser.url + "/admin/")
     wait = WebDriverWait(browser, 5)
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.LOGO))
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.PANEL_TITLE))
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.INPUT_USERNAME))
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.INPUT_PASSWORD))
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.FORGOTTEN_PASSWORD_BUTTON))
-    wait.until(EC.visibility_of_element_located(LoginAdminPage.LOGIN_BUTTON))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.LOGO))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.PANEL_TITLE))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.INPUT_USERNAME))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.INPUT_PASSWORD))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.FORGOTTEN_PASSWORD_BUTTON))
+    assert wait.until(EC.visibility_of_element_located(LoginAdminPage.LOGIN_BUTTON))
